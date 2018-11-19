9e0ce95327f0de4e7150ca39baa6f3fed789250d
root <dldouglaslopes@hotmail.com>
2018-11-19 11:49:02 -0300

    cap 9 finalizado

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -5,8 +5,20 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._negociacoes = new Negociacoes(function(model){
-            this._negociacoesView.update(model);
+        const self = this;
+        this._negociacoes = new Proxy(new Negociacoes(), {
+            get(target, prop, receiver){
+                if(typeof(target[prop]) == typeof(Function) && ['adiciona', 'esvazia']
+                    .includes(prop)){
+                        return function(){
+                            console.log(`"${prop}" disparou a amardilha`);
+                            target[prop].apply(target, arguments);
+                            self._negociacoesView.update(target);
+                        }
+                }else{
+                    return target[prop];
+                }
+            }
         });
         this._negociacoesView = new NegociacoesView('#negociacoes');
         this._negociacoesView.update(this._negociacoes);
@@ -18,8 +30,7 @@ class NegociacaoController {
     adiciona(event){
         event.preventDefault();
         this._negociacoes.adiciona(this._criaNegociacao()); 
-        this._mensagem.texto = 'Negociação adicionada com sucesso';
-        this._negociacoesView.update(this._negociacoes);  
+        this._mensagem.texto = 'Negociação adicionada com sucesso';  
         this._mensagemView.update(this._mensagem);     
         this._limpaFormulario();
     }
@@ -41,7 +52,6 @@ class NegociacaoController {
 
     apaga(){
         this._negociacoes.esvazia();
-        this._negociacoesView.update(this._negociacoes);
         this._mensagem.texto = 'Negociações apagadas com sucesso';
         this._mensagemView.update(this._mensagem);
     }
--- client/app/ui/views/View.js
+++ client/app/ui/views/View.js
@@ -8,6 +8,6 @@ class View {
     }
 
     template(model){
-        throw new Error('Você precisa implementar o método template');
+        throw new Error('Você precisa implementar o método template');  
     }
 }
