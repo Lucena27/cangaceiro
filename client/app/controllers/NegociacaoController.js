9be6c9d0bd364d3f0ad5c93ba1901e4079a4eac2
root <dldouglaslopes@hotmail.com>
2018-11-22 11:41:04 -0300

    cap 13.5 pag 296

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -17,6 +17,8 @@ class NegociacaoController {
             new MensagemView('#mensagemView'),
             'texto'
         );
+
+        this._service = new NegociacaoService();
     }
 
     adiciona(event){
@@ -59,24 +61,13 @@ class NegociacaoController {
     }
 
     importaNegociacoes(){
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', 'negociacoes/semana');
-
-        xhr.onreadystatechange = () => {
-            if(xhr.readyState == 4){
-                if(xhr.status == 200){
-                    JSON
-                        .parse(xhr.responseText)
-                        .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
-                        .forEach(negociacao => this._negociacoes.adiciona(negociacao));
-
-                    this._mensagem.texto = 'Negociações importadas com sucesso!';
-                }else{
-                    console.log(xhr.responseText);
-                    console.log('Não foi possível obter as negociações da semana.');
-                }
-            }
-        };
-        xhr.send();
-    }
+       this._service.obtemNegociacoesDaSemana()
+       .then(
+           negociacoes => {
+               negociacoes.forEach(negociacao => this._negociacoes.adiciona(negociacao));
+               this._mensagem.texto = 'Negociações importadas com sucesso';
+           },
+           err => this._mensagem.texto = err
+       );
+    }    
 }
---
+++ client/app/domain/negociacao/NegociacaoService.js
@@ -0,0 +1,21 @@
+class NegociacaoService {
+    constructor(){
+        this._http = new HttpService();
+    }
+
+    obtemNegociacoesDaSemana(){
+        return this._http  
+            .get('negociacoes/semana')
+            .then(
+                dados => {
+                    const negociacoes = dados.map(objeto =>
+                        new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor));
+                        return negociacoes;
+                    },
+                    err => {
+                        throw new Error('Não foi possível obter as negociaões');
+                    }
+            );    
+    }
+}
+    
